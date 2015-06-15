9716b430d076efa6f79345c15fead0dbc35fad2a
Tomasz Poradowski <tomasz@poradowski.net>
2015-06-15 22:42:34 +0200

    new FormClick event fired when any control is clicked (except close)

--- Toaster/BaseForm.cs
+++ Toaster/BaseForm.cs
@@ -8,24 +8,55 @@ namespace Toaster
 {
 	public partial class BaseForm : Form
 	{
-		private Timer timer = new Timer();
+		#region Public Fields
+
+		/// <summary>
+		/// The dismissed indicator.
+		/// </summary>
+		public Boolean Dismissed = false;
+
+		#endregion Public Fields
+
+		#region Internal Fields
+
 		internal DateTime DateShown = DateTime.Now;
+
+		#endregion Internal Fields
+
+		#region Private Fields
+
 		private String iconUrl = "";
 
-		public String ToastIconUrl
-		{
-			get { return this.iconUrl; }
-			set
-			{
-				this.iconUrl = value;
-				if (!String.IsNullOrEmpty(this.iconUrl))
-				{
-					System.Net.WebRequest request = System.Net.WebRequest.Create(this.iconUrl);
-					using (System.Net.WebResponse response = request.GetResponse()) { this.NotificationIcon = new Bitmap(response.GetResponseStream()); }
-				}
-			}
-		}
+		private Bitmap NotificationIcon = null;
 
+		private Timer timer = new Timer();
+
+		#endregion Private Fields
+
+		#region Public Properties
+
+		/// <summary>
+		/// Gets or sets the body.
+		/// </summary>
+		/// <value>The body.</value>
+		public String Body { get { return this.bodyLabel.Text; } set { this.bodyLabel.Text = value; } }
+
+		/// <summary>
+		/// Gets or sets the duration.
+		/// </summary>
+		/// <value>The duration.</value>
+		public int Duration { get { return this.timer.Interval; } set { this.timer.Interval = value; } }
+
+		/// <summary>
+		/// Gets or sets the title.
+		/// </summary>
+		/// <value>The title.</value>
+		public String Title { get { return this.titleLabel.Text; } set { this.titleLabel.Text = value; } }
+
+		/// <summary>
+		/// Sets the toast icon.
+		/// </summary>
+		/// <value>The toast icon.</value>
 		public Icon ToastIcon
 		{
 			set
@@ -37,17 +68,37 @@ namespace Toaster
 			}
 		}
 
-		public String Title { get { return this.titleLabel.Text; } set { this.titleLabel.Text = value; } }
+		/// <summary>
+		/// Gets or sets the toast icon URL.
+		/// </summary>
+		/// <value>The toast icon URL.</value>
+		public String ToastIconUrl
+		{
+			get
+			{
+				return this.iconUrl;
+			}
+			set
+			{
+				this.iconUrl = value;
+				if (!String.IsNullOrEmpty(this.iconUrl))
+				{
+					System.Net.WebRequest request = System.Net.WebRequest.Create(this.iconUrl);
+					using (System.Net.WebResponse response = request.GetResponse()) { this.NotificationIcon = new Bitmap(response.GetResponseStream()); }
+				}
+			}
+		}
 
-		public String Body { get { return this.bodyLabel.Text; } set { this.bodyLabel.Text = value; } }
+		#endregion Public Properties
 
-		public int Duration { get { return this.timer.Interval; } set { this.timer.Interval = value; } }
+		#region Public Constructors
 
-		private Bitmap NotificationIcon = null;
-		public Boolean Dismissed = false;
-
-		#region "Constructors"
-
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BaseForm"/> class.
+		/// </summary>
+		/// <param name="title">The title.</param>
+		/// <param name="body">The body.</param>
+		/// <param name="duration">The duration.</param>
 		public BaseForm(String title, String body, int duration = 15000)
 		{
 			InitializeComponent();
@@ -56,27 +107,26 @@ namespace Toaster
 			this.Duration = duration;
 		}
 
-		//public BaseForm(String title, String body, int duration = 15000) {
-		//    InitializeComponent();
-		//    this.titleLabel.Text = title;
-		//    this.bodyLabel.Text = body;
-		//    this.timer.Interval = duration;
-		//}
-		//public BaseForm(String title, String body, String iconUrl, int duration = 15000) {
-		//    InitializeComponent();
-		//    this.titleLabel.Text = title;
-		//    this.bodyLabel.Text = body;
-		//    this.IconUrl = new Uri(iconUrl);
-		//    this.timer.Interval = duration;
-		//    System.Net.WebRequest request = System.Net.WebRequest.Create(this.IconUrl.ToString());
-		//    using (System.Net.WebResponse response = request.GetResponse()) { this.NotificationIcon = new Bitmap(response.GetResponseStream()); }
-		//}
+		#endregion Public Constructors
 
-		#endregion "Constructors"
+		#region Public Events
 
+		/// <summary>
+		/// Occurs when any control (except closeLabel) is clicked.
+		/// </summary>
+		public event EventHandler<EventArgs> FormClick;
+
+		#endregion Public Events
+
+		#region Internal Methods
+
+		/// <summary>
+		/// Fades out this form.
+		/// </summary>
+		/// <param name="duration">The duration.</param>
 		internal void FadeOut(int duration = 500)
 		{
-			var amount = (double)15 / duration;
+			var amount = (double) 15 / duration;
 			var fadeoutTimer = new Timer { Interval = 15, Enabled = true };
 			fadeoutTimer.Tick += delegate
 			{
@@ -85,6 +135,9 @@ namespace Toaster
 			};
 		}
 
+		/// <summary>
+		/// Repositions this form.
+		/// </summary>
 		internal void Reposition()
 		{
 			var newTop = Screen.PrimaryScreen.WorkingArea.Height - Toaster.Toast.ActiveToasts.Where(x => x.DateShown <= this.DateShown).Sum(x => x.Height + 5);
@@ -101,6 +154,27 @@ namespace Toaster
 			animateTimer.Start();
 		}
 
+		#endregion Internal Methods
+
+		#region Protected Methods
+
+		/// <summary>
+		/// Called when any form control is clicked (except for special closeLabel).
+		/// </summary>
+		protected void OnFormClick()
+		{
+			if (FormClick != null)
+			{
+				FormClick(this, EventArgs.Empty);
+			}
+		}
+
+		/// <summary>
+		/// Paints the background of the control.
+		/// </summary>
+		/// <param name="e">
+		/// A <see cref="T:System.Windows.Forms.PaintEventArgs"/> that contains the event data.
+		/// </param>
 		protected override void OnPaintBackground(PaintEventArgs e)
 		{
 			e.Graphics.InterpolationMode = InterpolationMode.HighQualityBilinear;
@@ -125,8 +199,15 @@ namespace Toaster
 			e.Graphics.DrawRectangle(borderPen, 0, 0, this.Width - 1, this.Height - 1);
 		}
 
-		#region "Event Handlers"
+		#endregion Protected Methods
 
+		#region Private Methods
+
+		/// <summary>
+		/// Handles the Load event of the BaseForm control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
 		private void BaseForm_Load(object sender, EventArgs e)
 		{
 			// TODO: Determine which screen to show it on. For now we will show it on the primary screen.
@@ -149,27 +230,48 @@ namespace Toaster
 			timer.Tick += delegate { timer.Enabled = false; this.FadeOut(); };
 		}
 
+		/// <summary>
+		/// Handles the MouseClick event of the BaseForm control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
+		private void BaseForm_MouseClick(object sender, MouseEventArgs e)
+		{
+			OnFormClick();
+			this.FadeOut(150);
+		}
+
+		/// <summary>
+		/// Handles the Click event of the closeLabel control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
 		private void closeLabel_Click(object sender, EventArgs e)
 		{
 			this.Dismissed = true;
 			this.FadeOut(150);
 		}
 
+		/// <summary>
+		/// Handles the MouseEnter event of the closeLabel control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
 		private void closeLabel_MouseEnter(object sender, EventArgs e)
 		{
 			closeLabel.BackColor = Color.FromArgb(225, 225, 225);
 		}
 
+		/// <summary>
+		/// Handles the MouseLeave event of the closeLabel control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
 		private void closeLabel_MouseLeave(object sender, EventArgs e)
 		{
 			closeLabel.BackColor = Color.FromArgb(240, 240, 240);
 		}
 
-		private void BaseForm_MouseClick(object sender, MouseEventArgs e)
-		{
-			this.FadeOut(150);
-		}
-
-		#endregion "Event Handlers"
+		#endregion Private Methods
 	}
 }
